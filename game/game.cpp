90586d3605d8b902eb9a5858b73004e9d8dfc32e
Andrew Guschin <saintruler@gmail.com>
2021-07-05 12:17:58 +0400

    Добавил рендеринг спрайтов. (#2)
    * Добавил рендеринг спрайтов.
    
    * Добавил глобальные координаты отрисовки.

--- game/game.cpp
+++ game/game.cpp
@@ -8,10 +8,65 @@
 struct Transform : Component
 {
     sf::Vector2f position = {0, 0};
+    sf::Vector2f scale = {1, 1};
+};
+struct Sprite : Component
+{
+    std::string assetPath;
+    bool loaded = false;
+    sf::Image image;
+    sf::Texture texture;
+    sf::Sprite sprite;
+};
+struct Camera : Component
+{
+    sf::Vector2f scale = {1, 1};
 };
 struct Player : Component {};
 
+
 /* Systems */
+
+void
+render(GameState *state, Storage *storage, const Entity id)
+{
+    auto camera = storage->getComponent<Camera>(state->currentCamera);
+    auto camTransform = storage->getComponent<Transform>(state->currentCamera);
+    auto camPos = camTransform->position;
+
+    auto t = storage->getComponent<Transform>(id);
+    auto spr = storage->getComponent<Sprite>(id);
+
+    if (!spr->loaded)
+    {
+        spr->image.loadFromFile(spr->assetPath);
+        spr->texture.loadFromImage(spr->image);
+        spr->sprite.setTexture(spr->texture);
+        sf::IntRect rect = { 0, 0
+                , (int) spr->image.getSize().x
+                , (int) spr->image.getSize().y };
+        spr->sprite.setTextureRect(rect);
+        spr->loaded = true;
+    }
+
+    sf::Vector2f screenPos = { (t->position.x - camPos.x) * camera->scale.x
+                             , (camPos.y - t->position.y) * camera->scale.y };
+
+    screenPos += (sf::Vector2f) state->window->getSize() * 0.5f;
+
+    auto spriteSize = (sf::Vector2f) spr->texture.getSize();
+    spriteSize.x *= t->scale.x * camera->scale.x;
+    spriteSize.y *= t->scale.y * camera->scale.y;
+    screenPos -= spriteSize * 0.5f;
+
+    auto screenScale = t->scale;
+    screenScale.x *= camera->scale.x;
+    screenScale.y *= camera->scale.y;
+
+    spr->sprite.setPosition(screenPos);
+    spr->sprite.setScale(screenScale);
+    state->window->draw(spr->sprite);
+}
 
 void
 movePlayer(GameState *state, Storage *storage, const Entity id)
@@ -43,8 +98,11 @@ void
 initializeEngine(GameState *state, Storage *storage)
 {
     storage->registerComponent<Transform>();
+    storage->registerComponent<Sprite>();
+    storage->registerComponent<Camera>();
     storage->registerComponent<Player>();
 
+    storage->registerSystem(render, {TYPE(Transform), TYPE(Sprite)});
     storage->registerSystem(movePlayer, {TYPE(Transform), TYPE(Player)});
 }
 
@@ -54,6 +112,15 @@ void
 loadScene(const Config *config, const std::string& sceneName, GameState *state, Storage *storage)
 {
     Entity e1 = storage->createEntity();
-    storage->addComponent<Transform>(e1);
+    auto e1_t = storage->addComponent<Transform>(e1);
+    e1_t->scale = {0.1f, 0.1f};
+    auto spr = storage->addComponent<Sprite>(e1);
+    spr->assetPath = "assets/images/cube.jpg";
     storage->addComponent<Player>(e1);
+
+    Entity camera = storage->createEntity();
+    storage->addComponent<Transform>(camera);
+    auto cam = storage->addComponent<Camera>(camera);
+    cam->scale = {1, 1};
+    state->currentCamera = camera;
 }
