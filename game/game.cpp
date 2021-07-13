aee39bd9460f470706e29163c9834daa5bda9410
Roman Stain <stain.roman@mail.ru>
2021-07-13 11:16:24 +0400

    Убрал функцию updateCollider и перенёс её реализацию в функцию collision.

--- game/game.cpp
+++ game/game.cpp
@@ -2,6 +2,7 @@
 #include <engine/base.h>
 
 #include <set>
+#include <cmath>
 #include <iostream>
 
 /* Components */
@@ -28,14 +29,20 @@ struct Collider : Component
 {
     float width = 0;
     float height = 0;
+    // NOTE(Roma): Разница между центром объекта и центром его коллайдера.
     sf::Vector2f deltaCenter = {0, 0};
+    // NOTE(Roma): Левый нижний и правый верхний углы, необходимые для обнаружения пересечения двух коллайдеров.
     sf::Vector2f leftDownCorner = {0, 0};
     sf::Vector2f rightUpCorner = {0, 0};
+    // NOTE(Roma): Список всех объектов, с которыми пересекается данный.
     std::set<Entity> collisionList;
+    // NOTE(Roma): Разрешена ли коллизия: если нет, то объекты не смогут пересекаться.
+    bool allowCollision = false;
 };
 struct Physics : Component
 {
     float speed = 0;
+    // NOTE(Roma): Вектор направления движения.
     sf::Vector2f dir = {0, 0};
 };
 /* Systems */
@@ -70,49 +77,28 @@ render(GameState *state, Storage *storage, const Entity id)
 
 }
 
-void
-updateCollider(GameState *state, Storage *storage, const Entity id)
-{
-    for (Entity id : storage->usedIds)
-    {
-        auto coll = storage->getComponent<Collider>(id);
-        if (coll != nullptr)
-        {
-            auto t = storage->getComponent<Transform>(id);
-            auto tPos = t->position;
-            auto tSc = t->scale;
-            auto dc = coll->deltaCenter;
-            float w = coll->width * 0.5 * tSc.x;
-            float h = coll->height * 0.5 * tSc.y;
-            // NOTE(Roma) : Вычисление размеров коллайдера относительно центра коллайдера, его длины и ширины
-            coll->leftDownCorner = {tPos.x + dc.x - w, tPos.y + dc.y - h};
-            coll->rightUpCorner = {tPos.x + dc.x + w, tPos.y + dc.y + h};
-        }
-    }
-}
-
 // TODO(Roma): Нужно исправить коллизию при перемещении по диагонали
 void
 pushOut(GameState *state, Storage *storage, const Entity id)
 {
-    for (Entity id : storage->usedIds)
+    for (Entity ent_id : storage->usedIds)
     {
-        auto coll = storage->getComponent<Collider>(id);
-        if (coll != nullptr && coll->collisionList.size() != 0)
+        auto coll = storage->getComponent<Collider>(ent_id);
+        if (coll != nullptr && !coll->collisionList.empty() && !coll->allowCollision)
         {
             for (auto id2 : coll->collisionList)
             {
                 auto coll2 = storage->getComponent<Collider>(id2);
-                auto t = storage->getComponent<Transform>(id);
+                auto t = storage->getComponent<Transform>(ent_id);
                 auto t2 = storage->getComponent<Transform>(id2);
                 sf::Vector2f c = {t->position.x + coll->deltaCenter.x, t->position.y + coll->deltaCenter.y};
                 sf::Vector2f c2 = {t2->position.x + coll2->deltaCenter.x, t2->position.y + coll2->deltaCenter.y};
                 sf::Vector2f dC = {c.x - c2.x, c.y - c2.y};
-                auto p = storage->getComponent<Physics>(id);
+                auto p = storage->getComponent<Physics>(ent_id);
                 if (p->dir.x != 0 && p->dir.y != 0)
                 {
-                    t->position.x += p->speed * dC.x;
-                    t->position.y += p->speed * dC.y;
+                        t->position.x += p->speed * dC.x;
+                        t->position.y += p->speed * dC.y;
                 }
                 else
                 {
@@ -156,22 +142,49 @@ movePlayer(GameState *state, Storage *storage, const Entity id)
 void
 collision (GameState *state, Storage *storage, const Entity id)
 {
+    // NOTE(Roma): У первого объекта берутся коллайдер и трансформ для обновления координат углов коллайдера
     auto c = storage->getComponent<Collider>(id);
+    auto t = storage->getComponent<Transform>(id);
+    auto tPos = t->position;
+    auto tSc = t->scale;
+    auto dc = c->deltaCenter;
+    float w = c->width * 0.5f * tSc.x;
+    float h = c->height * 0.5f * tSc.y;
+    // NOTE(Roma): К центру объекта прибавляется разница между ним и центром коллайдера, чтобы вычислить координаты
+    // последнего. Затем прибавляется или убавляется половина длины и ширины коллайдера с учётом масштаба,
+    // чтобы вычислить левый нижний или правый верхний уголы.
+    c->leftDownCorner = {tPos.x + dc.x - w, tPos.y + dc.y - h};
+    c->rightUpCorner = {tPos.x + dc.x + w, tPos.y + dc.y + h};
     auto lC = c->leftDownCorner;
     auto rC = c->rightUpCorner;
+
     for (Entity id2 : storage->usedIds)
     {
+        // NOTE(Roma): Берётся коллайдер другого объекта, и если он есть и этот объект не совпадает с текущим,
         auto c2 = storage->getComponent<Collider>(id2);
         if (c2 != nullptr && id2 != id)
         {
+            // то обновляем координаты углов его коллайдера
+            auto t2 = storage->getComponent<Transform>(id2);
+            auto tPos2 = t2->position;
+            auto tSc2 = t2->scale;
+            auto dc2 = c2->deltaCenter;
+            float w2 = c2->width * 0.5f * tSc2.x;
+            float h2 = c2->height * 0.5f * tSc2.y;
+            c2->leftDownCorner = {tPos2.x + dc2.x - w2, tPos2.y + dc2.y - h2};
+            c2->rightUpCorner = {tPos2.x + dc2.x + w2, tPos2.y + dc2.y + h2};
             auto lC2 = c2->leftDownCorner;
             auto rC2 = c2->rightUpCorner;
+            // NOTE(Roma): и проверяем по углам, не пересекаются ли они.
             if (rC.x < lC2.x || lC.x > rC2.x || rC.y < lC2.y || lC.y > rC2.y)
             {
+                // В случае, когда не пересекаются, нужно удалить в списке пересечений первого объекта
+                // второй объект. Аналогично со списком второго.
                 c->collisionList.erase(id2);
                 c2->collisionList.erase(id);
                 continue;
             }
+            // Иначе, поскольку они пересекаются, добавляем объекты в списки друг друга.
             c->collisionList.insert(id2);
             c2->collisionList.insert(id);
 
@@ -193,7 +206,7 @@ initializeEngine(GameState *state, Storage *storage)
 
     storage->registerSystem(render, {TYPE(Transform), TYPE(Sprite)});
     storage->registerSystem(movePlayer, {TYPE(Transform), TYPE(Player)});
-    storage->registerSystem(updateCollider, {TYPE(Transform), TYPE(Sprite)});
+    //storage->registerSystem(updateCollider, {TYPE(Transform), TYPE(Sprite)});
     storage->registerSystem(collision, {TYPE(Collider), TYPE(Player)});
     storage->registerSystem(pushOut, {TYPE(Collider), TYPE(Physics), TYPE(Transform)});
 
@@ -209,8 +222,8 @@ loadScene(const Config *config, const std::string& sceneName, GameState *state, 
     storage->addComponent<Player>(e1);
     storage->addComponent<Physics>(e1);
     e1_t->scale = {0.1f, 0.1f};
-    auto spr = storage->addComponent<Sprite>(e1);
-    spr->assetPath = "assets/images/cube.jpg";
+    auto spr1 = storage->addComponent<Sprite>(e1);
+    spr1->assetPath = "assets/images/cube.jpg";
     auto p = storage->getComponent<Physics>(e1);
     p->speed = 0.1f;
 
@@ -239,12 +252,10 @@ loadScene(const Config *config, const std::string& sceneName, GameState *state, 
     }
     auto c = storage->addComponent<Collider>(e1);
     auto c2 = storage->addComponent<Collider>(e2);
-
-    c->deltaCenter = {0, 0};
-    c->width = spr->image.getSize().x;
-    c->height = spr->image.getSize().y;
-    c2->width = spr2->image.getSize().x;
-    c2->height = spr2->image.getSize().y;
+    c->width = (float) spr1->image.getSize().x;
+    c->height = (float) spr1->image.getSize().y;
+    c2->width = (float) spr2->image.getSize().x;
+    c2->height = (float) spr2->image.getSize().y;
 
     Entity camera = storage->createEntity();
     storage->addComponent<Transform>(camera);
