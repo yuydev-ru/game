b4af8ef0c3cedc766bb3effaaea4ffcebea17b90
nikita <niki.c2000@yandex.ru>
2021-07-30 00:25:18 +0400

    Функция боевой системы и структуры для нее

--- game/game.cpp
+++ game/game.cpp
@@ -21,6 +21,86 @@ struct Player : Component
     }
 };
 
+
+struct  Enemy : Component
+{
+
+    static Component *
+    deserialize(Parser &parser)
+    {
+        return new Enemy;
+    }
+};
+
+struct Health : Component
+{
+    int health = 100;
+
+    static Component *
+    deserialize(Parser &parser)
+    {
+        return new Health;
+    }
+};
+
+struct Weapon: Component
+{
+
+    int damage = 1;
+    float cooldown = 5;
+
+    static Component *
+    deserialize(Parser &parser)
+    {
+        return new Weapon;
+    }
+};
+
+
+void
+combatSystem(GameState *state, Storage *storage, const Entity id)
+{
+    auto a = storage->getComponent<Health>(id);
+    auto b = storage->getComponent<Transform>(id);
+    auto c = storage->getComponent<Weapon>(id);
+    float radius = 30000;
+
+    c->cooldown -= state->deltaTime;
+
+    if (state->axes["attack"] == true )
+    {
+        if (c->cooldown > 0)
+        {
+           // std::cout<<"your cd:"<<c->cooldown<<"\n";
+            return;
+        }
+        else
+        {
+
+            for(auto now_id: storage->usedIds)
+            {
+                auto now_typ = storage->getComponent<Enemy>(now_id);
+
+                if (now_typ!=NULL)
+                {
+                    auto now_tran = storage->getComponent<Transform>(now_id);
+                    float dist = pow((b->position.x-now_tran->position.x),2) + pow((b->position.y-now_tran->position.y),2);
+
+                    if (dist < radius){
+                        auto now_hp_en = storage->getComponent<Health>(now_id);
+
+                        now_hp_en->health -= c -> damage;
+                        c->cooldown = 5;
+
+                        std::cout << "attack" << " " << now_hp_en->health << "\n";
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 void
 movePlayer(GameState *state, Storage *storage, const Entity id)
 {
@@ -60,5 +140,10 @@ void
 initializeEngine(GameState *state, Storage *storage)
 {
     storage->registerComponent<Player>("Player");
+    storage->registerComponent<Enemy>("Enemy");
+    storage->registerComponent<Health>("Health");
+    storage->registerComponent<Weapon>("Weapon");
+
+    storage->registerSystem(combatSystem, {TYPE(Player), TYPE(Health), TYPE(Weapon), TYPE(Transform)});
     storage->registerSystem(movePlayer, {TYPE(Transform), TYPE(Player), TYPE(Collider), TYPE(Physics)});
 }
